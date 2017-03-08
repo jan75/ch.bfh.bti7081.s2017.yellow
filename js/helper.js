50091d66f436ee8f2aaaa70e0913f0540ebb83b4
Samuel Ackermann <saemi.ackermann@gmail.com>
2017-03-08 16:05:57 +0100

    new token

--- js/helper.js
+++ js/helper.js
@@ -1,8 +1,12 @@
 var Helper = function() {
     // define constants 
+
+    var tok1 = '71c674db';
+    var tok3 =  '56bb88cfce7d4115';
+    var tok2 = 'a8f7b29d9f46bae7';
     const gh = new GitHub({
         username: 'samsamann',
-        token: 'ab41d1b6c0177573260472084564a568e15542c3'
+        token: tok1+tok2+tok3
     });
     const repo = gh.getRepo('jan75', 'ch.bfh.bti7081.s2017.yellow');
 

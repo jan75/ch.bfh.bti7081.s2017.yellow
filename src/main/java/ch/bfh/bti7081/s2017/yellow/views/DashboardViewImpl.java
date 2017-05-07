f8ab461d0fd3401fe1335023c8a600729fdf6875
simon <simon@Simons-MacBook-Pro.local>
2017-05-07 12:14:16 +0200

    Dashboard presenter and view added Please follow the TODO statements for further information

---
+++ src/main/java/ch/bfh/bti7081/s2017/yellow/views/DashboardViewImpl.java
@@ -0,0 +1,17 @@
+package ch.bfh.bti7081.s2017.yellow.views;
+
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * Container for DashboardItems, Vertically layouted.
+ * @author iSorp
+ */
+public class DashboardViewImpl extends VerticalLayout implements DashboardView {
+
+    public DashboardViewImpl() {
+    }
+
+    /*
+    * TODO: Maybe some listeners are needed?
+    * */
+}

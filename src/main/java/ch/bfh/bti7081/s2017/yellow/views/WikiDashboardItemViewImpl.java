88f1aa2a47bc2e3db8843b6a93d61554c9874fe3
brunm1 <marcdenis.bruenisholz@students.bfh.ch>
2017-05-09 20:20:03 +0200

    add basic components with dummy data for wiki dash board item

--- src/main/java/ch/bfh/bti7081/s2017/yellow/views/WikiDashboardItemViewImpl.java
+++ src/main/java/ch/bfh/bti7081/s2017/yellow/views/WikiDashboardItemViewImpl.java
@@ -1,10 +1,28 @@
 package ch.bfh.bti7081.s2017.yellow.views;
 
+import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
 public class WikiDashboardItemViewImpl extends VerticalLayout implements WikiDashboardItemView{
 
 	public WikiDashboardItemViewImpl() {
-		
+		Label wikiEntryTitle = new Label("Wiki Titel");
+        addComponent(wikiEntryTitle);
+        Label wikiEntryContent = new Label("Wiki content: Lorem Ipsum "
+        		+ "is simply dummy text of the printing and typesetting "
+        		+ "industry. Lorem Ipsum has been the industry's standard "
+        		+ "dummy text ever since the 1500s, when an unknown printer "
+        		+ "took a galley of type and scrambled it to make a type "
+        		+ "specimen book. It has survived not only five centuries, "
+        		+ "but also the leap into electronic typesetting, remaining "
+        		+ "essentially unchanged. It was popularised in the 1960s "
+        		+ "with the release of Letraset sheets containing Lorem "
+        		+ "Ipsum passages, and more recently with desktop publishing "
+        		+ "software like Aldus PageMaker including versions of "
+        		+ "Lorem Ipsum.");
+        addComponent(wikiEntryContent);
+        Label wikiEntryAuthor = new Label("- Hans Fritz");
+        addComponent(wikiEntryAuthor);
 	}
+	
 }

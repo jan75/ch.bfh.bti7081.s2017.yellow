9584c1dd6a60a68489e17b687ae857f8bc20b795
brunm1 <marcdenis.bruenisholz@students.bfh.ch>
2017-05-19 00:18:02 +0200

    Add db initialization in init method. remove xml config. add db config to crudRepositoryImpl.

--- src/main/java/ch/bfh/bti7081/s2017/yellow/views/MainView.java
+++ src/main/java/ch/bfh/bti7081/s2017/yellow/views/MainView.java
@@ -10,6 +10,9 @@ import ch.bfh.bti7081.s2017.yellow.presenters.PlanningDashboardItemPresenter;
 import ch.bfh.bti7081.s2017.yellow.repositories.CrudRepository;
 import ch.bfh.bti7081.s2017.yellow.repositories.CrudRepositoryImpl;
 import ch.bfh.bti7081.s2017.yellow.services.SimpleServiceImpl;
+
+import java.sql.SQLException;
+
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.UI;
@@ -17,25 +20,25 @@ import com.vaadin.ui.VerticalLayout;
 
 public class MainView extends UI {
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        VerticalLayout rootLayout = new VerticalLayout();
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		try {
+			CrudRepositoryImpl.initDbConnection();
+		} catch (SQLException e) {
+			System.out.println("Could not start db connection");
+			throw new RuntimeException(e);
+		}
+		
+		VerticalLayout rootLayout = new VerticalLayout();
 
-        MainMenuView view = new MainMenuView();
-        new MainMenuPresenter(view);
-        rootLayout.addComponent(view);
+		MainMenuView view = new MainMenuView();
+		new MainMenuPresenter(view);
+		rootLayout.addComponent(view);
 
-        DashboardView dashboardView = new DashboardViewImpl();
-        new DashboardPresenter(dashboardView);
-        rootLayout.addComponent(dashboardView);
+		DashboardView dashboardView = new DashboardViewImpl();
+		new DashboardPresenter(dashboardView);
+		rootLayout.addComponent(dashboardView);
 
-        setContent(rootLayout);
-        
-    	ContactBookEntry entry = new ContactBookEntry(new Person("name", "name2"), "079234987234987234");
-		CrudRepository<Storable> crud = new CrudRepositoryImpl<>();
-    	crud.save(entry);
-    	
-    	
-    	
-    }
+		setContent(rootLayout);
+	}
 }

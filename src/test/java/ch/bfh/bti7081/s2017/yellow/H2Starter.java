0d8efdae3adec9d6141808b7668b30e8d326be40
brunm1 <marcdenis.bruenisholz@students.bfh.ch>
2017-05-18 22:57:21 +0200

    Two different starters: 1. H2 playground 2. Quick browser access to db

--- src/test/java/ch/bfh/bti7081/s2017/yellow/H2Starter.java
+++ src/test/java/ch/bfh/bti7081/s2017/yellow/H2Starter.java
@@ -2,22 +2,47 @@ package ch.bfh.bti7081.s2017.yellow;
 
 import java.sql.SQLException;
 
+import org.h2.tools.Console;
+
 /**
- * Run as separate application.
- * It starts a TCP server and connects to it via browser,
- * creating an in-memory database.
+ * Run inner classes as a "Java Application".
+ * This class provides quick access to H2 databases for developers.
  */
 public class H2Starter {
-	public static void main(String... args){
-		try {
-			org.h2.tools.Console.main("-tcp", "-web", "-pg");
-			org.h2.tools.Console.main(
-					"-url", "jdbc:h2:tcp://localhost/mem:db1",
-					"-driver", "org.h2.Driver",
-					"-user", "sa",
-					"-password", "");
-		} catch (SQLException e) {
-			e.printStackTrace();
+	
+	private static void createH2Server() throws SQLException {
+		Console.main("-tcp");
+	}
+	
+	private static void openConnectionInBrowser() throws SQLException {
+		Console.main(
+			"-url", "jdbc:h2:tcp://localhost/mem:db1",
+			"-driver", "org.h2.Driver",
+			"-user", "sa",
+			"-password", "");
+	}
+	
+	/**
+	 * Creates a new TCP server and connects to it via browser.
+	 * The login happens automatically.
+	 * Can be used to play around with a H2 database.
+	 */
+	static class H2PlaygroundStarter {
+		public static void main(String... args) throws SQLException {
+			createH2Server();
+			openConnectionInBrowser();
+		}
+	}
+	
+	/**
+	 * Opens the h2 browser tool and connects automatically.
+	 * This is handy to inspect an already running database.
+	 * Note that it creates a web server that connects to the db.
+	 * Close the program manually to stop the web server.
+	 */
+	static class H2BrowserToolStarter  {
+		public static void main(String... args) throws SQLException {
+			openConnectionInBrowser();
 		}
 	}
 }

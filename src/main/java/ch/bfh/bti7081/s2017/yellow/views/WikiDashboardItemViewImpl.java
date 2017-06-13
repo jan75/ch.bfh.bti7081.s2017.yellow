f3d7f633365e992dc400a07a82b8ddd828776e70
brunofernandez1 <brunofernandez@msn.com>
2017-06-13 20:29:38 +0200

    added accordion

--- src/main/java/ch/bfh/bti7081/s2017/yellow/views/WikiDashboardItemViewImpl.java
+++ src/main/java/ch/bfh/bti7081/s2017/yellow/views/WikiDashboardItemViewImpl.java
@@ -20,7 +20,7 @@ public class WikiDashboardItemViewImpl extends VerticalLayout implements WikiDas
         		+ "Ipsum passages, and more recently with desktop publishing "
         		+ "software like Aldus PageMaker including versions of "
         		+ "Lorem Ipsum.");
-        wikiEntryContent.setWidth("200px");
+        wikiEntryContent.setWidth("300px");
         addComponent(wikiEntryContent);
         Label wikiEntryAuthor = new Label("- Hans Fritz");
         addComponent(wikiEntryAuthor);
